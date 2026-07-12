41a605b5e6573af1bd9e9be6db13df634d3e3c36
AndrijShwed <a_shwed@ukr.net>
2026-07-12 21:54:18 +0300

    2454

---
+++ ClaudeComBook.Desktop/Views/LandView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ClaudeComBook.Desktop;
+
+public partial class LandView : Window
+{
+    public LandView()
+    {
+        InitializeComponent();
+    }
+}
